2e251f9bc5059a922dc2af31e0549b59d0198764
ICHURT <133573231+ICHURT@users.noreply.github.com>
2023-05-14 23:03:52 +0800

    Update index.cjs

--- config/index.cjs
+++ config/index.cjs
@@ -56,7 +56,7 @@ const USER_CONFIG = {
   CALLBACK_USERS: [
     {
       name: '自己',
-      // 使用微信测试号：自己的微信id，扫码关注你的微信测试号后生成的一段字符串，在测试号后台能看到
+      // 使用微信测试号：自己的微信id，扫码关注你的微测试号后生成的一段字符串，在测试号后台能看到
       id: 'oT5ZN5s6RE26je-Jm-kQXvAwyihU',
     }
   ],
