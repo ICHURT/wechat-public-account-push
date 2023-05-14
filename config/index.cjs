8ed22ae59c73b0f261ecf8876f24075ba744beeb
ICHURT <133573231+ICHURT@users.noreply.github.com>
2023-05-14 20:24:15 +0800

    Update index.cjs

--- config/index.cjs
+++ config/index.cjs
@@ -51,13 +51,13 @@ const USER_CONFIG = {
 
 
   // 【推送完成提醒】模板id, 用来看自己有没有发送成功的那个模板
-  CALLBACK_TEMPLATE_ID: '',
+  CALLBACK_TEMPLATE_ID: 'AMy60JuwRrVq71IUpQ5edsJg2FUbxI1Fxj_O_J_itTw',
 
   CALLBACK_USERS: [
     {
       name: '自己',
       // 使用微信测试号：自己的微信id，扫码关注你的微信测试号后生成的一段字符串，在测试号后台能看到
-      id: '',
+      id: 'oT5ZN5s6RE26je-Jm-kQXvAwyihU',
     }
   ],
 
