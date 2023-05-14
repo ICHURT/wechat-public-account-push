332007d6f4f339664bfe00055cb6c58e78b89c9a
ICHURT <133573231+ICHURT@users.noreply.github.com>
2023-05-14 23:13:06 +0800

    Update index.cjs

--- config/index.cjs
+++ config/index.cjs
@@ -19,9 +19,9 @@ const USER_CONFIG = {
   USERS: [
     {
       // 想要发送的人的名字
-      name: '宝贝',
+      name: '亲爱的添添宝贝',
       // 使用微信测试号：扫码关注你的微信测试号后生成的一段字符串，在测试号后台能看到
-      id: 'oT5ZN5s6RE26je-Jm-kQXvAwyihU',
+      id: 'oT5ZN5s6RE26je-Jm-kQXvAwyihU' 'oT5ZN5i9c9Xu_SZBl84Fyw5_WAb0',
       // 使用微信测试号：你想对他发送的模板消息的模板ID
       useTemplateId: 'mHocl2U5XFmKjnx77KHqcPmBVsi3OzOHbXBi2N7IzhA',
       // 新历生日, 仅用作获取星座运势, 格式必须为MM-DD
