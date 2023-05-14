6eadccaa6daa7d177d46a54dcb56f8285db485d4
ICHURT <133573231+ICHURT@users.noreply.github.com>
2023-05-14 19:31:07 +0800

    Update index.cjs

--- config/index.cjs
+++ config/index.cjs
@@ -8,22 +8,22 @@
 const USER_CONFIG = {
 
   // 使用微信测试号：公众号APP_ID
-  APP_ID: '',
+  APP_ID: 'wx4dc83f032e4fb510',
 
   // 使用微信测试号：公众号APP_SECRET
-  APP_SECRET: '',
+  APP_SECRET: 'aad3b4c04ce1ba9e479614e545a04d8a',
 
-  PROVINCE: '广东',
-  CITY: '惠州',
+  PROVINCE: '北京',
+  CITY: '北京',
 
   USERS: [
     {
       // 想要发送的人的名字
       name: '宝贝',
       // 使用微信测试号：扫码关注你的微信测试号后生成的一段字符串，在测试号后台能看到
-      id: '',
+      id: 'oT5ZN5i9c9Xu_SZBl84Fyw5_WAb0',
       // 使用微信测试号：你想对他发送的模板消息的模板ID
-      useTemplateId: '',
+      useTemplateId: 'AMy60JuwRrVq71IUpQ5edsJg2FUbxI1Fxj_O_J_itTw',
       // 新历生日, 仅用作获取星座运势, 格式必须为MM-DD
       horoscopeDate: '12-27',
       festivals: [
@@ -57,7 +57,7 @@ const USER_CONFIG = {
     {
       name: '自己',
       // 使用微信测试号：自己的微信id，扫码关注你的微信测试号后生成的一段字符串，在测试号后台能看到
-      id: '',
+      id: 'oT5ZN5s6RE26je-Jm-kQXvAwyihU',
     }
   ],
 
